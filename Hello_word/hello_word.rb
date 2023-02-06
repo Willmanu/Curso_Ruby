8bc48be812d977ad72c1bb087068bc887adb02e8
willmanu <pamanuwill@gamil.com>
2023-02-06 18:17:00 -0300

    hello Word

---
+++ Hello_word/hello_word.rb
@@ -0,0 +1,6 @@
+# para executar o código instale a extensão run code.
+# apos isso clique Ctrl + Alt + n e o código sera mostrado no output.
+# ou clique em view, Command Palette e Run Code.
+# No terminal navegue até o arquivo .rb escreva ruby + nome_arquivo.rb
+mensagem = 'hello Word!'
+puts(mensagem)
