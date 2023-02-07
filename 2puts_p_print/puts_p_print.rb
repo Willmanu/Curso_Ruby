3e880c9ffc0e27934298d527a675f99edc411de2
willmanu <pamanuwill@gamil.com>
2023-02-07 19:47:47 -0300

    estudo ruby incial

---
+++ 2puts_p_print/puts_p_print.rb
@@ -0,0 +1,9 @@
+# Todos os três métodos servem para imprimir na tela uma mensagem.
+# Podem ser usados com parêntese ou não.
+
+# O puts abaixo imprime quebrando a linha, será apresenta em colunas cada string
+puts ['A', 'B', 'C']
+
+# O p e o Print imprime as letras uam ao lado do outa
+p ['A', 'B', 'C']
+print ['A', 'B', 'C']
---
+++ 3estrutura_funcao/estrutura_funcao.rb
@@ -0,0 +1,7 @@
+def somar(x , y)
+	x + y
+end
+somar(1,2)
+
+resultado = somar
+print (resultado)
