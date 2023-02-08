5525db6ed2e2e125f39fa7317dd8f344ce4248b6
willmanu <pamanuwill@gamil.com>
2023-02-08 19:58:54 -0300

    Generations

---
+++ 4generators/generators.rb
@@ -0,0 +1,19 @@
+# Generators são geradores
+# O framework Reails é baseado em geradores
+# Se for necessário gerar um controller ou model ou view por exemplo,
+# preciso dar um comando para gera-los, cada um tem o seu comando.
+# Para gerar um nova aplicação basta digitar 
+# rails new + endereço do diretório onde será criada a aplicação + nome_app
+# exemplo: o endereço desta pasta que estou é:
+# /home/william/workspace/estudoruby/Curso_Ruby/4generators/
+# se eu quiser criar uma aplicação dentro desta pasta seria assim:
+# rails new /home/william/workspace/estudoruby/Curso_Ruby/4generators/ + nome_app
+# A partir deste comando o rails vai gerar um diretório dentro desta pasta 
+# com varias outras pasta que formam este app
+# pastas de controllers, model, view, gems etc.
+# Após ser criado esta aplicação, basta entrar neste diretório do novo app
+# e executar o comando rails server ou rails s
+# O que vai acontecer é que o rails vai levantar o servidor
+# O rails cria o próprio servidor bem simples que permite utilizar para testar a aplicação
+# Basta pegar a porta onde está a aplicação escrever localhost: + numero_porta
+# no navegador e com certeza aparecer esta aplicação
