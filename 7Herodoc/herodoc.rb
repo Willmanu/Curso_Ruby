77ab8ba0f3964d94e8f690760f11774e55c42e8e
Willmanu <pamanuwill@gmail.com>
2023-02-10 08:06:12 -0300

    Herodoc verificando erro azul

--- 7Herodoc/herodoc.rb
+++ 7Herodoc/herodoc.rb
@@ -1,17 +1,19 @@
 mensagem = <<~TXT
-Usado para se criar um texto grande, ou seja,
-quando se precisa fazer muitas cadeia de caracteres
-Declaramos uma variável chamada mensagem que recebe o textão,
-digitamos os símbolos: <<~ depois um delimitador, exemplo: TXT, abrindo e outro fechando
-A cadeia de caractere, ou seja, o textão fica entre o delimitador.
 
-Delimitador 
-Significado de delimitador
-Adjetivo
-Que delimita, que determina as fronteiras ou os limites de alguma coisa.
-Substantivo masculino
-Aquilo ou que delimita, que impõe limites.
+  Usado para se criar um texto grande, ou seja,
+  quando se precisa fazer muitas cadeia de caracteres
+  Declaramos uma variável chamada mensagem que recebe o textão,
+  digitamos os símbolos: <<~ depois um delimitador, exemplo: TXT, abrindo e outro fechando
+  A cadeia de caractere, ou seja, o textão fica entre o delimitador.
+
+  Delimitador
+  Significado de delimitador
+  Adjetivo
+  Que delimita, que determina as fronteiras ou os limites de alguma coisa.
+  Substantivo masculino
+  Aquilo ou que delimita, que impõe limites.
+
 TXT
 
 puts mensagem
-puts "A quantidade de caracteres no texto é: #{mensagem.length} "# a quantidade de carctere é 504
+puts "A quantidade de caracteres no texto é: #{mensagem.length}" # a quantidade de caractere é 504.
