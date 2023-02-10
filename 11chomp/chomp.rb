6e71dd8495d80aefc3688555370c7bed13dddf47
Willmanu <pamanuwill@gmail.com>
2023-02-10 16:16:18 -0300

    chomp

---
+++ 11chomp/chomp.rb
@@ -0,0 +1,7 @@
+=begin
+Estou fazendo um programa em Ruby e preciso fazer uma pergunta ao user.
+Bom, posso usar o método get pata ler a entrada do usuário
+=end
+
+name = gets.chomp
+puts "Olá #{name}, prazer em conhecê-lo!"
