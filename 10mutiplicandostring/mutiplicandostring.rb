4ed250b35be034892a5b41b54b2287c84d1301a7
Willmanu <pamanuwill@gmail.com>
2023-02-10 16:04:18 -0300

    multiplicacaostring

---
+++ 10mutiplicandostring/mutiplicandostring.rb
@@ -0,0 +1,23 @@
+=begin
+  Imagine precisar usar o - para separar linhas de texto
+  Exemplo:
+         Multiplicação de string
+    --------------------------------------
+	    Conteúdo
+		
+	Acima os traços estão separando o titulo da palavra Conteúdo.
+	No caso esse 38 traços foram feitos a mão.
+	para que o código faça este exemplo para mim, o que eu preciso e por o traço entre aspas duplas
+	e multiplicar por 38.
+	Exemplo: 
+=end
+
+p 'Multiplicação de string'
+p '-' * 38
+p 'Conteúdo'
+=begin
+  o que foi impresso é o mesmo resultado feito a mão no exemplo dentro do comentário
+         Multiplicação de string
+    --------------------------------------
+         Conteúdo
+=end
