e319cf8d554d5a5c18e5a50e4633b859781465de
Willmanu <pamanuwill@gmail.com>
2023-02-14 10:20:55 -0300

    definindo array

---
+++ 8definindoarray/definindoarray.rb
@@ -0,0 +1,21 @@
+=begin
+  Para definir array em Ruby faça o seguinte:
+  a frente da variável que vai ser do tipo array abra e feche chaves -> []
+   exemplo:
+=end
+
+meuarray = []
+p meuarray.class # mostra na tela que o tipo da variável é array
+
+=begin
+  Para fazer uma lista dentro do array, cada valor deve ser separado por virgula.
+	exemplo:
+=end
+
+meuarray = ['Emanuel', 'William', 'Pamella', 'Yuri', 10, 11, false, nil]
+p meuarray # a impressão fica exatamente mostrando os valores igual está acima
+
+=begin
+  A lista pode ser composta por tipos diferentes,
+  perceba que na impressão saiu string, i
+=end
