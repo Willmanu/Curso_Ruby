5fa720dfe15fff3a6d8b2adc0877c356e267d862
Willmanu <pamanuwill@gmail.com>
2023-02-14 12:55:25 -0300

    Estudando capitalize

---
+++ 17primeiraletramaiusculacapitalize/capitalize.rb
@@ -0,0 +1,9 @@
+=begin
+  Pode haver situações em que se precise que uma string
+  deva ser sua 1º letra em maiúscula.
+  Para fazer isso com uma string toda em minúscula usa-se o método .capitalize
+  exemplo:
+=end
+
+mensagem = 'minha primeira mensagem'
+p mensagem.capitalize # será impresso a palavra minha assim-> "Minha primeira mensagem"
