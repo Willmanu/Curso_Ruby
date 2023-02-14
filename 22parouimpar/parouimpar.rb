defa72f3fde4f076d7371ae126919b9173797e79
Willmanu <pamanuwill@gmail.com>
2023-02-14 19:56:26 -0300

    Ver se um numero é impar ou par .even? ou .odd?

---
+++ 22parouimpar/parouimpar.rb
@@ -0,0 +1,13 @@
+=begin
+  Uma forma de saber se um numero é para ou impar é fazer a divisão por 2.
+  Se o resto da divisão for zero o numero é par.
+  Ver o numero para com .even
+  Ver o numero impar com .odd
+  Para essa verificação é só mandar imprimir com uma interrogação.
+  Sendo verdade o retorno é true.
+  Exemplo:
+=end
+
+numero = 2
+number2 = 3
+p numero.even?
