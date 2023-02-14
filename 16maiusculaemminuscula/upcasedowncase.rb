b4e08bab1620fb49d448e4596641eea54e24520d
Willmanu <pamanuwill@gmail.com>
2023-02-14 11:39:20 -0300

    Upcase e Downcase minúscula em maiúscula e vise versa

---
+++ 16maiusculaemminuscula/upcasedowncase.rb
@@ -0,0 +1,28 @@
+=begin
+  Transformar maiúsculo e minúsculo e vice-versa
+Com .upcase qualquer palavra transformo em MAIÚSCULA
+Com .downcase qualquer palavra transformo em MINÚSCULA
+
+exemplo:
+=end
+
+name_minuscula = 'william'
+NAME_MAIUSCULA = 'WILLIAM'
+
+p name_minuscula
+p NAME_MAIUSCULA
+
+p '-' * 60
+
+p name_minuscula.upcase
+p NAME_MAIUSCULA.downcase
+
+=begin
+  a impressão fica assim:
+  
+  "william"
+  "WILLIAM"
+  "------------------------------------------------------------"
+  "WILLIAM"
+  "william"
+=end
