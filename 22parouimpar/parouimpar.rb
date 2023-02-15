e486d532f8ca5dd89039c24b73608c2dd29d72a8
Willmanu <pamanuwill@gmail.com>
2023-02-15 11:47:07 -0300

    Para ou impar

--- 22parouimpar/parouimpar.rb
+++ 22parouimpar/parouimpar.rb
@@ -1,13 +1,62 @@
 =begin
-  Uma forma de saber se um numero é para ou impar é fazer a divisão por 2.
-  Se o resto da divisão for zero o numero é par.
-  Ver o numero para com .even
-  Ver o numero impar com .odd
-  Para essa verificação é só mandar imprimir com uma interrogação.
-  Sendo verdade o retorno é true.
-  Exemplo:
+              Verificando se um numero é para.
+  Existem situações, que vamos precisar verificar se um numero que vai
+  ser obtido é par ou impar, para determinados resultado.
+  Umas das formas para saber é dividindo este numero por dois, e se o resto da divisão
+  for 0 então saberemos que é par, caso contrário é impar.
+
+	Para ver o resto da divisão usamos o % na operação.
+	Exemplo:
 =end
 
-numero = 2
-number2 = 3
-p numero.even?
+puts 'Digite um numero'
+
+numero = gets.to_i # Tudo que é obtido do teclado do pc é string. 
+                   # Portanto precisa fazer a conversão para Integer ou Float
+
+if numero == 0
+  puts 'O numero digitado é zero e ele não é divisível. Por favor digitar outro numero.'
+
+  # aqui falta interromper o programa
+end
+
+resultado = numero % 2 # usando o porcentagem para ver o resto da divisão
+
+if resultado == 0
+	puts "O número digitado é: #{numero} portanto é par"
+  
+  else
+	  puts "O número digitado é: #{numero} portanto é impar"
+
+end
+
+=begin
+
+Uma outra forma bem mais simples de se fazer essa tarefa é usar os métodos
+.even para o par e .odd para o impar
+
+=end
+
+=begin
+  Perceba que não precisa fazer conta e verificar.
+  Somente fazer as perguntas e depois fazer a logica para exibir na tela.
+=end
+
+puts 'Digite um numero'
+
+numero = gets.to_i
+if numero.zero?
+  puts 'O numero digitado é zero e ele não é divisível. Por favor digitar outro numero.'
+
+  # aqui falta interromper o programa
+end
+  
+
+if numero.even?
+  puts "O número digitado é: #{numero} portanto é par"
+
+else
+
+  puts "O número digitado é: #{numero} portanto é impar"
+
+end
