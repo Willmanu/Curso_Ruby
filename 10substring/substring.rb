675ec4f037d542916abd3fb0d03f5a7de8b214a9
Willmanu <pamanuwill@gmail.com>
2023-02-16 18:18:32 -0300

    Aletrando linha de código errada

--- 10substring/substring.rb
+++ 10substring/substring.rb
@@ -10,7 +10,7 @@ name_ = 'Cadu'
 p name_[0, 2]
 
 =begin
-  O acontece acima é: dentro do colchete eu digo que quero acessar da posição 0 e até a
+  O que acontece acima é: dentro do colchete eu digo que quero acessar da posição 0 e até a
   posição 2.
   O que vai ser impresso é 'Ca' sendo C a posição 0 e 'a' a posição 2.
   Se eu por dentro do colchete[0,3] o que vai ser impresso é 'Cad'
