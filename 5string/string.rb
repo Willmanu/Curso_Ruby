401930e6abaaf11d2ff1cdf2f75549904f1e8b41
Willmanu <pamanuwill@gmail.com>
2023-02-16 18:01:19 -0300

    Adicionando informção sobre %q

--- 5string/string.rb
+++ 5string/string.rb
@@ -27,7 +27,7 @@ puts texto.class # o que se imprimi é String
 
 =begin
   Sabemos que uma string é escrita contendo aspas simples (prefirível se não for escrever
-  interpolação) ou aspas duplas (preferível quando escrever interpolação.) 
+  interpolação) ou aspas duplas (preferível quando escrever interpolação.)
 
   E o que fazer se em uma mensagem que formos escrever para o usuário tiver que usar
   esses caracteres especiais?
@@ -35,56 +35,64 @@ puts texto.class # o que se imprimi é String
   aspas duplas e simples.
   It's a "string" with 'quotes'
 
+  Se escrever essa mensagem entre aspas duplas ou simples, teremos conflitos.
+  Cada aspas simples ou dupla abre e fecha, assim o programa sabe que é um testo.
+  Sempre será assim uma abre e outra fecha, porém em casos de palavra como It's
+  que tem uma aspas simples separando o It do s, o programa entende que essa aspa
+  simples após o It, fecha a primeira que foi aberta desconsiderando o resto da frase.
+
+  Para não temos esses conflito precisamos usar caracteres de escape.
+  Caracteres de escapes serve para isso: escapar de conflitos como esses ditos em string.
+  Usa uma barra inclinada para esquerda \ ates do caractere que desejar escapar
+  Por exemplo:
+=end
+
+frase = 'It\'s a "string" with \'quotes\' '
+puts frase
+
+=begin
+
+  Perceba que o que foi impresso na tela é: It's a "string" with 'quotes', ou seja,
+  exatamente a frase com as palavras que usam aspas simples e duplas sem conflitos.
+
+  Como eu quero que a frase traga as aspas simples entre o It e s, a barra vai na frente
+  da aspas simples para traze-la. Da mesma forma na palavra 'quotes', para trazes
+  as aspas simples a barra vai a frente de cada aspa indicando que é para traze-la.
+  Na aspas duplas não é necessário por a barra, porque se trata aqui abrindo e fechando
+  a frase, aspas simples e elas não entram em conflitos.
 
 
+  Para tornar mais fácil essa questão, podemos ao invés de usar caractere de escape o %q()
+  Em Ruby, %q() é uma notação para criar uma string contendo um bloco de texto sem a
+  necessidade de escapar caracteres especiais como aspas simples, aspas duplas e
+  barras invertidas. %q() é chamado de "quote operator" ou "operador de citação".
+  Exemplo:
+=end
+
+frase = %q(It's a "string" with 'quotes')
+puts frase
+
+=begin
+  Perceba que o que foi impresso na tela é: It's a "string" with 'quotes', ou seja,
+  exatamente a frase com as palavras que usam aspas simples e duplas sem conflitos.
+
+  O () é um delimitador abrindo e fechando, quando se usa esse delimitador (),
+  no texto não poderá haver a outra metade do parentese fechando -> ')'
+  porque se não o programa entendera que este proximo fecha com o primeiro e o
+  resto do texto ficara def ora da impressão gerando conflito.
+
+  Por essa razão esse delimitador  pode ser substituído por outros caracteres delimitadores,
+  como %q{}, %q[], %q!!, etc. Isso é útil quando a string contém parênteses ou outros
+  caracteres que interferem com a sintaxe do %q(). 
+  Por exemplo:
 
 =end
 
-=begin
-      O %q
-  Em Ruby, %q() é uma forma de delimitar uma string literal que é usada para
-  criar uma string sem a necessidade de escapar caracteres especiais.
-  É uma forma de criar uma string sem interpolação de variáveis ou expressões embutidas.
-  O %q é seguido por um par de parênteses, onde você pode colocar o conteúdo da string.
-
-Por exemplo, %q(foo bar) é equivalente a uma string literal 'foo bar'.
-No entanto, %q() é especialmente útil quando a string contém muitas aspas simples ou duplas,
-evitando a necessidade de escapar cada uma delas.
-Por exemplo, %q(It's a "string" with 'quotes') produz a string It's a "string" with 'quotes'.
-
-Também existe uma variação de %q() chamada %Q(), que é semelhante, mas permite interpolação
-de variáveis e expressões embutidas.
-
-=end
-
-
-texto = %q(12345 meu texto é o melhor, é top 10 levando nota 100.0000)
-puts texto # o que se imprimi é String
-puts texto.class # o que se imprimi é String
+texto = %q{'Isso é um exemplo de' "string" usando parênteses ( ) no texto.}
+puts texto
 
 =begin
-Em Ruby, %q() é uma notação para criar uma string contendo um bloco de texto sem a necessidade de escapar caracteres especiais como aspas simples, aspas duplas e barras invertidas. %q() é chamado de "quote operator" ou "operador de citação".
-
-O %q é seguido de parênteses contendo o texto que se deseja incluir na string. Por exemplo:
-
-c
-Copy code
-texto = %q(Isso é um exemplo de string usando o operador de citação em Ruby.)
-puts texto
-O código acima produzirá a saída:
-
-go
-Copy code
-Isso é um exemplo de string usando o operador de citação em Ruby.
-O %q() pode ser substituído por outros caracteres delimitadores, como %q{}, %q[], %q!!, etc. Isso é útil quando a string contém parênteses ou outros caracteres que interferem com a sintaxe do %q(). Por exemplo:
-
-c
-Copy code
-texto = %q{Isso é um exemplo de string usando parênteses ( ) no texto.}
-puts texto
-O código acima produzirá a saída:
-
-go
-Copy code
-Isso é um exemplo de string usando parênteses ( ) no texto.
+  O que será impresso é:
+  Isso é um exemplo de string usando parênteses ( ) no texto.
+  Perceba que foi usado Chaves com delimitador.
 =end
