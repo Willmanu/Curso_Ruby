89d587908a9fa5461151f970402adf4486f0330c
Willmanu <pamanuwill@gmail.com>
2023-02-16 11:40:02 -0300

    Refazendo este porque apaguei com um comando errado

--- 3estrutura_funcao/estrutura_funcao.rb
+++ 3estrutura_funcao/estrutura_funcao.rb
@@ -1,4 +1,23 @@
-def somar(x,y)
-	puts x + y
+=begin
+  Para definir uma função:
+  1º escreva é def para abrir
+  2º escreva o nome da função
+  3º abra e feche parentese, e dentro deste vão os parâmetros
+  4º abaixo disso que foi feito acima, ou seja, na proxima linha vai o bloco de código
+  5º feche com end
+  6º chame a função pelo seu nome, e passe os parâmetros para realizar a função.
+  
+
+  No exemplo abaixo é uma função que faz a soma de valore. Perceba que dentro do parentese
+tenho as variáveis que vão receber os números que serão somados.
+  
+Fora da função, ou seja, após o end chamei a função pelo seu nome
+e passei os valores que quero a soma
+Os valores entram nas variáveis que tem o nome de valor1 e valor2, são somados e o resultado
+é impresso na tela conforma o código.
+=end
+
+def somar(valor1, valor2)
+  puts valor1 + valor2
 end
-somar(1,2)
+somar(1, 2)
