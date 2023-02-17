86ecce084c8e87a2d5ef8508f1939e5e044590c2
Willmanu <pamanuwill@gmail.com>
2023-02-17 17:54:39 -0300

    Mais informação sobre array diverças formas de inseriri valor ~

--- 24array/array.rb
+++ 24array/array.rb
@@ -17,17 +17,85 @@ array_2 = Array.new # O .new é um método que instancia novo objeto daquela cla
 p array_2.class
 
 =begin
-  Para incluir valor no array se faz de duas formas:
-  nome_array << valor
-  ou 
-  nome_array.append(valor)
+  Em Ruby, é possível inserir valores em um array de diversas maneiras além de usar
+  o sinal de igual, abre e fecha chaves e os valeres a serem inseridos quando mais de um
+  são separados por virgula.
+  meu_array = [1,2,3]
+  
+  Seguem abaixo algumas das principais formas:
+
+  Exemplo com o push
+
+  Utilizando o método push: o método push adiciona um ou mais elementos ao final do array.
+  Exemplo:
 =end
 
-array_1 << 1
+array = [1, 2, 3]
+array.push(4)
+puts array # [1, 2, 3, 4]
+
+
+
+array_1 << 1 # ao inserir mais de um valor inicie com [] e separe por virgula
 array_1.append(2)
-p array_1 # aqui é impresso [1, 2]
-p array_1.length # aqui é impresso 2
+array_1 << :William
+p array_1 # aqui é impresso [1, 2, :William]
+p array_1.length # aqui é impresso 3
+p array_1.size # aqui é impresso 3
 
 =begin
-  A lista de itens colocado no array, pode crescer a vontade.
+  Perceba que o array recebe valores de tipos diferentes: inteiro, Simbolo et.
+  Não se defini array com um tipo especifico em Ruby. 
+  A lista de itens colocado no array, pode crescer a vontade a medida
+  que é inserido novos valores.
+
+  Quando os valores entram no array, é criado indices para acessar esses valores. 
+  Os indices começam em 0.
+  Veja o exemplo de acesso.
 =end
+
+p array_1 [1] # aqui o que é impresso é 2
+p array_1 [0] # aqui o que é impresso é 1
+p array_1 [2] # aqui o que é impresso é :William
+
+=begin
+  Métodos Uteis quando se trabalha com array
+  Exemplo:
+=end
+
+p array_1.length # mostra o tamanho do array
+
+p array_1.size # mostra o tamanho do array igual o length esse é o mais usado
+
+p array_1.empty? # verifica se o array está vazio retorna tru ou false.
+
+p array_2.first # pega o primeiro valor
+
+p array_1.last # pega o último valor
+
+=begin
+  Em resumo, size e length em Ruby têm o mesmo comportamento e podem ser usados de forma
+  intercambiável, mas size é mais comum e disponível em mais classes de coleções.
+  
+  Abaixo estou somando indices de dois array diferente
+=end
+
+array_2 = [1, 2, 3]
+
+resultado = array_1 + array_2
+
+puts resultado 
+=begin
+  o que vai ser impresso é :
+  1
+  2
+  William
+  1
+  2
+  3
+  ou seja foi acrescentado os valores do dois array em resultado
+  E resultado por ter recebido os valores agora é do tipo array.
+=end
+
+p resultado.class # o que é impresso é Array
+p resultado.size
