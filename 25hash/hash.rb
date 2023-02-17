4929683d28de998de9ad8a74943c66859a084dee
Willmanu <pamanuwill@gmail.com>
2023-02-17 17:22:35 -0300

    Hash

---
+++ 25hash/hash.rb
@@ -0,0 +1,18 @@
+=begin
+
+  Hash é uma estrutura de chave-valor.
+  Em outras linguagem são chamadas ou de Hashes Map ou Dicionários.
+  O Hash funciona como no Array, ou seja, é uma variável que guarda mais de um valor,
+  e com isso conseguimos criar listas.
+
+  No Array quando colocamos valores, cada valor tem sua posição na lista começando em 0.
+  Quando procuramos algo dentro desse array, o programa percorre a lista inteira ate achar
+  esse índice.
+  Exemplo:
+
+=end
+
+lista = ['1, 2, 3, 4, 5, 6, 7, 8, 9, 10']
+p lista[1]
+
+
