bfc2d0885f955f17ceef80e38b4e6ca3ca5abce1
Willmanu <pamanuwill@gmail.com>
2023-02-19 17:11:18 -0300

    Estunado nil

---
+++ 27nilounull/nilounull.rb
@@ -0,0 +1,21 @@
+=begin
+
+nil para o ruby significa o nada absoluto.
+Em Ruby, a palavra-chave usada para representar a ausência de um valor é nil, não null.
+nil é um valor especial que representa a falta de um valor.
+É semelhante a null em outras linguagens de programação, mas difere em alguns aspectos
+importantes.
+
+
+Por exemplo, em Ruby, nil é considerado um valor falso, o que significa que ele é avaliado
+como false em uma expressão booleana.
+Além disso, ao contrário de outras linguagens de programação, em Ruby é possível chamar
+métodos em objetos nil sem causar um erro, o que pode ser útil em algumas situações.
+
+Imagina pegarmos um input de um usuário.
+E só queremos atribuir um valor a essa variável se ela estiver sem valor.
+
+=end
+
+valor
+puts valor
