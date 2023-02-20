162ff6c3899bcdcfeb5e00b4551eeccbbfa2920c
Willmanu <pamanuwill@gmail.com>
2023-02-19 22:07:34 -0300

    Iniciando estudo de case

---
+++ 28case/case.rb
@@ -0,0 +1,18 @@
+=begin
+
+  Em Ruby, "case" é uma estrutura de controle condicional que permite testar uma variável contra uma
+  série de valores possíveis e executar um bloco de código correspondente ao primeiro valor que
+  corresponde.
+
+  Sitaxe: 
+  Abre com a palavra case
+  Fecha com a palavra end
+  No meio dos dois temos o when
+
+  case significa -> caso
+  when significa -> quando
+  end significa -> fim
+
+  Podemos ter entre a estrutura um else.
+
+=end
