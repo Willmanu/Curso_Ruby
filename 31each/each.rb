6cf4c0f28289a456940f03b29d416c3570a04678
Willmanu <pamanuwill@gmail.com>
2023-02-21 13:41:33 -0300

    estudo de each

---
+++ 31each/each.rb
@@ -0,0 +1,10 @@
+=begin
+  
+  Um laço que a comunidade Ruby usa no lugar do for.
+  each significa cada
+  O each é usado quando temos que ler uma lista, array ou hash.
+  Este laço ou como conhecido método, é usado para iterar sobre uma coleção de elementos,
+  como uma matriz (array) ou um hash, e executar um bloco de código para cada elemento na
+  coleção.
+
+=end
