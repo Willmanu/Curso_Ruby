8ec7d20acc6f8f818ffc5741e2159a2b51896bb8
Willmanu <pamanuwill@gmail.com>
2023-02-22 20:15:30 -0300

    Explicando como funciona o código

--- 36palindromo/palindromo.rb
+++ 36palindromo/palindromo.rb
@@ -31,3 +31,31 @@ else
     puts "Sua palavra é #{palavra} portanto não é palíndromo"
   end
 end
+
+=begin
+
+          Entendendo o código
+
+  O método sempre deixe nas primeiras linhas.
+  1º precisamos pegar a entrada da palavra digitada com o gets, o chomp tirando caracteres
+  especiais e strip retirando possíveis espaços a direita e a esquerda.
+
+  2º empty para validar a variável e ver se o usuário não clicou
+  em enter ou digitou espaços vazios. Se for esse o caso temos um loop que imprimi uma
+  mensagem avisando para digitar uma palavra. O loop fica até que ele digite mesmo uma palavra.
+
+  3º dentro do loop se for o caso de não digitar palavras precisamos obter a palavra digitada
+  usando o $stdin que representa um objeto global de entrada padrão do programa.
+
+  4º o e_palindromo ao mesmo tempo que chama a função, é a variável que recebe o return
+  da mesma
+
+  5º temos uma condicional para imprimir caso o retorno seja palindromo ou não.
+  
+  6º uma coisas importantíssima a se dizer é que dentro da função ou método, temos o downcase
+  método que transforma todas as palavras em minusculas. Assim se o usuário digitar a palavra
+  começando em maiúscula, q
+        
+
+
+=end
