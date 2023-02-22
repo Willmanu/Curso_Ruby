93219e3b87d4649076d3f7ddee2a7b285d133110
Willmanu <pamanuwill@gmail.com>
2023-02-22 16:45:25 -0300

    Palíndromo

---
+++ 36palindromo/palindromo.rb
@@ -0,0 +1,32 @@
+=begin 
+  
+  Palíndromo é uma palavra que quandro lida da esquerda para direita, quanto da direita para
+  esquerda, tem o mesmo significado
+  Por exemplo: Ovo, Osso, Radar etc.
+  
+  O objetivo aqui é definir um método que verifique se a palavra é palíndromo.
+  Usar o gets para pedir o input de dados e chamar o método
+  imprimir se é palíndromo ou não
+
+=end
+
+puts 'Escreva uma palavra e verificaremos se é palíndromo ou não'
+
+palavra = gets.strip
+
+if palavra.empty
+ 
+  while palavra.empty
+  
+	puts 'Não é uma palavra, por favor digite uma palavra'
+
+	palavra.gets.strip
+
+  end
+	
+
+end
+
+def palíndromo (palavra)
+
+end
