2f866793a6a5e83e6cd39f0b2dde8c7c9c03b48b
Willmanu <pamanuwill@gmail.com>
2023-02-23 18:48:16 -0300

    Ajustando o código criando NullViewContexte NullLookupContext

--- 37numeroemmoedareal/numeroemmoedareal.rb
+++ 37numeroemmoedareal/numeroemmoedareal.rb
@@ -8,10 +8,25 @@ if numero.empty? || numero.include?('\n') || numero.include?(' ')
     puts 'O que foi digitado não é um número, por favor digite um '
     numero = $stdin.gets.chomp
   end
-
 end
 
-av = action_view::Base.new
+class NullViewContext
+  def link_to(*args)
+    ''
+  end
+
+  def each
+    [].each
+  end
+end
+
+class NullLookupContext
+  def find_all(*args)
+    []
+  end
+end
+
+av = ActionView::Base.new(NullLookupContext.new, NullViewContext.new, nil)
 
 formatado = av.number_to_currency(numero.to_f, unit: 'R$', separator: ',', delimiter: '.')
 
