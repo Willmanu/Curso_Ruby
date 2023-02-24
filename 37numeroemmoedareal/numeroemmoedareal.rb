637ff1d461f7fbea4c79dea28ce790e9bf96f28c
Willmanu <pamanuwill@gmail.com>
2023-02-24 12:29:32 -0300

    Deu loop infinito fiz a correção

--- 37numeroemmoedareal/numeroemmoedareal.rb
+++ 37numeroemmoedareal/numeroemmoedareal.rb
@@ -3,8 +3,8 @@ require 'action_view'
 puts 'Digite um número'
 numero = gets.chomp.strip
 
-if numero.empty? || numero.include?('\n') || numero.include?(' ') || numero.numeric?
-  while numero.empty? || numero.include?('\n') || numero.include?(' ') || numero.numeric? do
+if numero.empty? || numero.include?("\n") || numero.include?(' ') || !numero.match?(/\a\d+\z/)
+  while numero.empty? || numero.include?("\n") || numero.include?(' ') || !(numero.downcase.match?(/\a\d+\z/)) do
     puts 'O que foi digitado não é um número, por favor digite um '
     numero = $stdin.gets.chomp
   end
