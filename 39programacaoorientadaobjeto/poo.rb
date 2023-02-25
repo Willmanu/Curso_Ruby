ebd4ea3ba2461509e6d75005488fafc52818da69
Willmanu <pamanuwill@gmail.com>
2023-02-25 13:51:44 -0300

    E antes de poo como era

--- 39programacaoorientadaobjeto/poo.rb
+++ 39programacaoorientadaobjeto/poo.rb
@@ -5,7 +5,7 @@ O criador de POO foi Alan Kay, um matemático e biólogo.
 Ele é apaixonado por educação e principalmente a educação de crianças. Com o conhecimento
 lógico que conquistou com a matemática e o conhecimento de biologia formou os grandes
 conceitos da POO.
-Este formulou o seguinte postulado: ano de 1970
+Este formulou o seguinte postulado: ano de 1970 nem existia graduação em informática
 "O computador ideal deve funcionar como um organismo vivo, isso é: cada célula se relaciona
 com outras a fim de alcançar um objetivo, mas cada uma funciona de forma autônoma.
 As células poderiam se agrupar para resolver um outro problema ou desempenhar outras funções."
@@ -16,7 +16,10 @@ lógico.
 A beleza de POO é que fica simples pensar em uma aplicação pensando no objeto que ela se
 dispõe a ser.
 
-Assim qualquer coisa que seja considerado um objeto, pode ser explicado baseados nos
-conceitos criados por Alan Kay para POO aplicado os conceitos de POO
+Assim qualquer coisa que seja considerado um objeto, pode se aplicar os conceitos criados
+por Alan Kay para POO.
+
+                                           Antes de POO
+Antes de poo tinhamos a programação estruturada e programação
 
 =end
