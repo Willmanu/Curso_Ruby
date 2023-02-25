f4a49114ac9b70d590f20f757015c0169eafbba2
Willmanu <pamanuwill@gmail.com>
2023-02-25 13:25:07 -0300

    Explicando POO

--- 39programacaoorientadaobjeto/poo.rb
+++ 39programacaoorientadaobjeto/poo.rb
@@ -1,7 +1,22 @@
 =begin
                                        POO
 POO é programação orientada a objetos.
+O criador de POO foi Alan Kay, um matemático e biólogo.
+Ele é apaixonado por educação e principalmente a educação de crianças. Com o conhecimento
+lógico que conquistou com a matemática e o conhecimento de biologia formou os grandes
+conceitos da POO.
+Este formulou o seguinte postulado: ano de 1970
+"O computador ideal deve funcionar como um organismo vivo, isso é: cada célula se relaciona
+com outras a fim de alcançar um objetivo, mas cada uma funciona de forma autônoma.
+As células poderiam se agrupar para resolver um outro problema ou desempenhar outras funções."
 
-  
+Perceba nesta palavras que são carregadas de conhecimentos em biologia, matemática e raciocínio
+lógico.
+
+A beleza de POO é que fica simples pensar em uma aplicação pensando no objeto que ela se
+dispõe a ser.
+
+Assim qualquer coisa que seja considerado um objeto, pode ser explicado baseados nos
+conceitos criados por Alan Kay para POO aplicado os conceitos de POO
 
 =end
