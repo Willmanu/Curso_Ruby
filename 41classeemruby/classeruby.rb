4c7eaca566d4bb27c897d693072a8902030a4678
Willmanu <pamanuwill@gmail.com>
2023-02-26 18:49:15 -0300

    Fazendo um objeto em ruby

--- 41classeemruby/classeruby.rb
+++ 41classeemruby/classeruby.rb
@@ -9,6 +9,22 @@ End
 1º palavra class
 2º em camel case o nome da classe
 3º finaliza com end
+4º no meio vai seus métodos/funções e atributos etc.
+
+Segundo a convecção do Ruby:
+- nomes de de variáveis são todos em minúsculos e separado por underline caso duas palavras
+Exemplo: MinhaVarável
+- nome de classe são cada palavra começando em maiúscula(Camel Case)
+Exemplo: NomeDaClasse
+
+
+                                      Objeto
+Sabemos que um objeto tem seus atributos, métodos/funções e é construído a partir da classe ()
+Para construir esse objeto em Ruby se faz assim:
+
+objeto = NomeDaClasse.new 
+
+.new é método que constrói uma nova
 
 
 =end
