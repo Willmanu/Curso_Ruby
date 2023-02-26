721fb262e0b8ddcd0a6b08fd1550522f682d4784
Willmanu <pamanuwill@gmail.com>
2023-02-26 18:25:46 -0300

    Inciando classe em Ruby

---
+++ 41classeemruby/classeruby.rb
@@ -0,0 +1,14 @@
+=begin
+
+Sabemos que classe é um modelo, ou uma planta de construção de casa etc.(veja a teoria no 40)
+Em ruby uma classe é definida dessa forma:
+
+class NomeDaCLasse
+End
+
+1º palavra class
+2º em camel case o nome da classe
+3º finaliza com end
+
+
+=end
