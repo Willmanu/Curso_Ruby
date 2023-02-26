c4fe0e2173afa1785b8c8f89fad1a58b35322f00
Willmanu <pamanuwill@gmail.com>
2023-02-26 13:53:58 -0300

    Finalizando o que é objeto

--- 39programacaoorientadaobjeto/poo.rb
+++ 39programacaoorientadaobjeto/poo.rb
@@ -42,34 +42,6 @@ Pode escrever, rabiscar, sublinhar, pintar etc.
 Estado:
 Nova, usada, com tinta, sem tinta, tampada ou destampada etc.
 
-                           Classe
-
-Classe é molde, ou seja, o modelo no qual vários objetos vão ser construído a partir desse
-modelo ou o esquema ou a planta de uma construção.
-Imagine construir varias casas populares. Para isso todas as casas vão ser construídas com
-uma única planta.
-Com relação a caneta teremos o mesmo molde para fazer varias de cores diferentes
-A caneta pronta é o objeto.
-
-
-Para construir uma classe precisamos responder 3 perguntas:
-
-1º Que coisas eu tenho?
-2º Que coisas eu faço?
-3º Como estou agora?
-
-Que coisas eu tenho
-Exemplo: Modelo, Cor, ponta, carga, tampa
-
-Que coisas eu faço
-Exemplo: Escrever, Rabiscar, Sublinhar, pintar, tampar, Destampar etc.
-
-Como estou agora
-Exemplo: Nova com 100% de carga tampada etc.
-ou velha, com 10% de carga sem tampa etc.
-
-
-
 
 O criador de POO foi Alan Kay, um matemático e biólogo.
 Ele é apaixonado por educação e principalmente a educação de crianças. Com o conhecimento
