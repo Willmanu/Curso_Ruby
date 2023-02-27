54f6136216e8f1d7e2fc2d66df1e5cf485daea1a
Willmanu <pamanuwill@gmail.com>
2023-02-27 09:06:22 -0300

    Falando de variavel objeto e variável objeto

--- 41classeemruby/classeruby.rb
+++ 41classeemruby/classeruby.rb
@@ -17,25 +17,39 @@ Exemplo: minha_varável
 - nome de classe são cada palavra começando em maiúscula(Camel Case)
 Exemplo: NomeDaClasse
 
+=end
+
+class NomeDaClasse # declaração da classe
+end
+
+
+=begin
 
                                       Objeto
 Sabemos que um objeto tem seus atributos, métodos/funções e é construído a partir da classe ()
 Para construir esse objeto em Ruby se faz assim:
 objeto = NomeDaClasse.new
 
-.new é método que constrói uma nova
+.new é método que constrói coisas
 
 =end
 
-class NomeDaClasse
-end
-
-objeto = MinhaClass.new
+objeto = NomeDaClasse.new
 
 p objeto.object_id # 60
 
 =begin
 
+Neste caso temos uma palavra chamada objeto recebendo a classe, recebendo o molde, o esquema.
+Dessa forma a variável objeto esta construindo a classe em memória, por conta do
+método .new
+Esta variável não é uma variável comum como declaramos para receber um valor como:
+string, inteiros, float etc.
+Este agora é o "objeto" que vai receber atributos, ou seja, características e métodos/funções
+que vão gerar as ações.
+
+
+E seu id é o 60
 
 =end
 
