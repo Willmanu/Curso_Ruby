938591985b70f88f8fb52c7909a4d6cf1eb00efb
Willmanu <pamanuwill@gmail.com>
2023-02-27 20:02:24 -0300

    Criei o objeto Cumprimentar

--- 42metodosvariaveisdentrodeobjetos/metodosvariaveisdentrodeobjeto.rb
+++ 42metodosvariaveisdentrodeobjetos/metodosvariaveisdentrodeobjeto.rb
@@ -1,5 +1,25 @@
 =begin
                                Métodos e variáveis dentro de objetos
 
+Neste exemplo vamos criar um objeto que comprimente uma pessoa.
+O propósito é passar o nome de uma pessoa e gerar o Ola! nomepessoa.
+=end
+
+class Cumprimentar
+
+  def imprimir_ola(nome)
+
+    puts "Olá! #{nome}"
+
+  end
+
+end
+
+objeto = Cumprimentar.new
+objeto.imprimir_ola('William')
+
+=begin
+
+
 
 =end
