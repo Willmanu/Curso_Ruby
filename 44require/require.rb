38800ee5aa752229dc20396274ab8b834e9485db
Willmanu <pamanuwill@gmail.com>
2023-03-01 16:22:48 -0300

    awesome_print

---
+++ 44require/require.rb
@@ -0,0 +1,14 @@
+=begin
+
+Em Ruby, require é uma palavra-chave usada para carregar bibliotecas ou módulos externos
+que contém configurações de classes, métodos e outras funcionalidades reutilizáveis no
+nosso código.
+Quando se usa o require, o interpretador Ruby procura o arquivo especificado na variável
+$LOAD_PATHe carrega o conteúdo desse arquivo para dentro do seu programa.
+
+=end
+
+
+
+objetocarro = Carro.new('Chevrolet', '2002', 'William P. Silva')
+objetocarro.carro
