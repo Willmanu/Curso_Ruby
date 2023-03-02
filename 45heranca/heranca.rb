a6f8e1b395dd2707120f967acb8312265dad1a0b
Willmanu <pamanuwill@gmail.com>
2023-03-02 12:53:05 -0300

    Hierarquia de class

--- 45heranca/heranca.rb
+++ 45heranca/heranca.rb
@@ -7,6 +7,7 @@ Para definir uma herança é usado o símbolo de menor < para informar de qual c
 herdar.
 Exemplo:
 
+
 class Animal                definição da classe
 end
 
@@ -15,6 +16,10 @@ end
 
 Suponhamos que esta classe Animal tenha um método chamado respirar. Quando a classe Cachorro
 herdar a classe Animal, este método respirar fará parte da classe Cachorro.
+
+
+
+	                                 Class Object do Ruby
 
 Se ao criar uma classe, não for especificado a herança desta, o Ruby automaticamente faz a
 herança desta classe criada sem especificação, da classe Object do próprio Ruby. Ou seja, uma
@@ -30,4 +35,14 @@ end                         a classe Animal recebe herança de Object
 
 class Cachorro < Animal     Definindo class Cachorro e herdando com o < a class Animal
 end
+
+
+                                Ruby e hierarquia de classe
+
+Quando chamamos um método, o Ruby verifica primeiramente se este método que foi chamado, existe
+na classe Object (salvo se passado o endereço do método).
+Se não encontrar o método em Object, o Ruby passa e examinar a hierarquia de classes procurando
+este método. Achando o trás se não informa erro.
+
+Digamos que temos 
 =end
