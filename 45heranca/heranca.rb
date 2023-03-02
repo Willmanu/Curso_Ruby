bfb8f9ff48447636f3789ecd341ed0aabc93d421
Willmanu <pamanuwill@gmail.com>
2023-03-02 11:44:13 -0300

    Explicando a henrança

---
+++ 45heranca/heranca.rb
@@ -0,0 +1,16 @@
+=begin
+                                          Herança
+Herança é quando uma classe herda as funcionalidades de outra.
+No Ruby uma classe herda apenas de uma outra. Herança múltiplas não é permitido no Ruby como
+existe em outras linguagem
+Para definir uma herança é usado o símbolo de menor < para informar de qual classe ela vai
+herdar.
+Exemplo:
+
+class Animal                definição da classe
+end
+
+class Cachorro < Animal     Definindo class Cachorro e herdando com o < a class Animal
+end
+
+=end
