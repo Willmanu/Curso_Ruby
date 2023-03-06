ef82262bf6f47c337839954d7fed04120df0b614
Willmanu <pamanuwill@gmail.com>
2023-03-05 21:30:17 -0300

    Incializando o exercicio para criar Conta bancaria

---
+++ 2poo/47exerciciocontabancaria/contabancaria.rb
@@ -0,0 +1,10 @@
+=begin
+
+-Criar uma classe responsável para representar a conta bancaria
+
+- Criar um método que permita transferir valor entre contas usando:
+Conta1.transferir(conta2,100) onde 100 é o valor que eu desejo transferir.
+
+-Plus: Criar um tipo de conta em que existe uma tarifa para se transferir dinheiro
+
+=end
