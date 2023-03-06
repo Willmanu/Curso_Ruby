efb06e1e48ef66057c4efb9368fa747d85fdc808
Willmanu <pamanuwill@gmail.com>
2023-03-06 09:50:36 -0300

    Criando conta bancaria

--- 2poo/47exerciciocontabancaria/contabancaria.rb
+++ 2poo/47exerciciocontabancaria/contabancaria.rb
@@ -1,5 +1,5 @@
 =begin
-
+                            Criar uma conta Bancaria
 -Criar uma classe responsável para representar a conta bancaria
 
 - Criar um método que permita transferir valor entre contas usando:
@@ -8,3 +8,16 @@ Conta1.transferir(conta2,100) onde 100 é o valor que eu desejo transferir.
 -Plus: Criar um tipo de conta em que existe uma tarifa para se transferir dinheiro
 
 =end
+
+# Esta representa uma conta bancaria
+class ContaBancaria
+  def dadospessoais(nome, agencia, conta)
+    @nome = nome
+    @agencia = agencia
+    @conta = conta
+  end
+
+  def conta1.transferir(contadestino, valortransferir)
+
+  end
+end
