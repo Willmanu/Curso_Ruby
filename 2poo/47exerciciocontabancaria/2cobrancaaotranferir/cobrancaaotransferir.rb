4ae0df66bb8e6f868c5f899f4e11d35eb9eda127
Willmanu <pamanuwill@gmail.com>
2023-03-06 16:11:29 -0300

    Superclasse imprimindo mensagem para o usuario com dados da tranferencia

--- 2poo/47exerciciocontabancaria/2cobrancaaotranferir/cobrancaaotransferir.rb
+++ 2poo/47exerciciocontabancaria/2cobrancaaotranferir/cobrancaaotransferir.rb
@@ -2,11 +2,18 @@ require '/home/william/workspace/estudoruby/Curso_Ruby/2poo/47exerciciocontabanc
 
 # Classe que gera um valor ao transferir dinheiro
 class CobrancaAoTransferir < ContaBancaria
-  def cobrancaaotransferir
-    contabancaria = ContaBancaria.new
-    contabancaria.dadospessoais
+  def initialize
+    @cobranca = ''
+    super
+  end
+
+  def dadospessoais
+    @cobranca = @valortransferir * 0.01.to_f
+    puts "O valo de #{@valortransferir} de transferência para a conta#{@contadestino}"\
+    " foi feito por #{@nome} que tem como agencia #{@agencia} e conta #{@conta}"
+    super
   end
 end
 
 cobrancaaotransferir = CobrancaAoTransferir.new
-cobrancaaotransferir.cobrancaaotransferir
+cobrancaaotransferir.dadospessoais
