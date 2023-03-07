3f5aeee6f281a48aebd3a9d3303ac3aa8985408f
Willmanu <pamanuwill@gmail.com>
2023-03-07 14:27:00 -0300

    Descrevendo o que o código faz, para mentalizar todos os passos

--- 2poo/47exerciciocontabancaria/3feitopeloprof/1principal.rb
+++ 2poo/47exerciciocontabancaria/3feitopeloprof/1principal.rb
@@ -1,6 +1,10 @@
 require './2poo/47exerciciocontabancaria/3feitopeloprof/feitopeloprof'
 require './2poo/47exerciciocontabancaria/3feitopeloprof/contacomtaxa'
 
+=begin
+O propósito aqui e fazer uam transferencia de uma conta para outra
+
+=end
 conta_william = ContaComTaxa.new('William', 100)
 
 conta_pessoa2 = ContaBancaria.new('pessoa2', 200)
@@ -18,3 +22,13 @@ p conta_pessoa2.saldo   #  aqui que é 250 recebendo 50 tem que ficar 300
 conta_william.transferir(conta_pessoa2, 60) # teste que tem que falhar
 p 'A conta do William'
 p conta_william.saldo
+
+=begin
+
+Neste arquivo principal iniciamos a ContaComTaxa e ContaBancaria
+ContaComTaxa tem esse nome porque será cobrado uma taxa de transferencia pela transferencia
+ContaComTaxa recebe por parametro o nome do dono da conta e o valor que ele tem em conta
+ContaBancaria recebe o dono da conta que vai receber a transferencia
+
+
+=end
--- 2poo/47exerciciocontabancaria/3feitopeloprof/contacomtaxa.rb
+++ 2poo/47exerciciocontabancaria/3feitopeloprof/contacomtaxa.rb
@@ -3,21 +3,24 @@ Este método transferir em ContaBancaria, recebe o Objeto instanciado conta_pess
 transferencia do William, que está em valor_transferencia.
 Esse valores foram passados para ContaBancaria pela classe principal
 
-Aqui o objetivo desta classe é redefinir o método transferir
-O método transferir aqui recebe os mesmo valores de que Contabancaria recebeu: conta_pessoa2 e
+Aqui o objetivo desta classe ContaComTaxa, é redefinir o método transferir
+O método transferir aqui recebe os mesmo valores que ContaBancaria recebeu: conta_pessoa2 e
 o valor_transferencia
 O super trás o mesmo comportamento da classe pai ContaBancaria
-Definindo Super na primeira linha do método
+Definindo Super na primeira linha do método, o programa trás aqui os valores que fores feitos
+na classe ContaBancaria, ou seja, o resultado de ContaBancaria classe pai está aqui.
+O método debitar é chamado e leva o valor de 2 reais que é a taxa a ser cobrado.
+Quando chega em ContaBancaria o valor inicial ja está com o debito do valor_transferir, e desse
+valor debitado retira a taxa que é 2 reais.
 
-com os mesmo pegando o valor_transferencia da
-classe ContaBancaria que vem da principal, e usar o método debitar para retirar o valor da taxa
-do valor que o william tem.
 =end
 
 # Esta classe trata de cobrar uma taxa de transferencia
 class ContaComTaxa < ContaBancaria
-  def transferir (pessoa2, valor_transferencia)
-    super(pessoa2, valor_transferencia)
-    debitar(2)
+  def transferir(pessoa2, valor_transferencia)
+    if saldo >= valor_transferencia
+      super(pessoa2, valor_transferencia)
+      debitar(2)
+    end
   end
 end
