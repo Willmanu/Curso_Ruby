94b06ba931f21aa26dac82b5136ee93850d5807c
Willmanu <pamanuwill@gmail.com>
2023-03-08 10:16:57 -0300

    Començando com um projeto rails

---
+++ 3criandoumprojeto/criandoumprojeto.rb
@@ -0,0 +1,15 @@
+=begin
+                                              Criando um projeto
+Rails é um Gem, ou seja, uma biblioteca
+Sua instalação pode ser feita pelo terminal com o comando:
+
+gems installed rails
+
+Após esse comando, o Rails baixa todas as dependências, as instala e instala também a biblioteca
+Após essa instalação é possível criar um projeto
+Indo no terminal e escrevendo:
+rails new nome_projeto, dando enter nesta linha, o rails cria as estruturas de diretórios dessa
+aplicação
+Cria-se um d
+
+=end
