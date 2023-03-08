15d17a83cf6d72f178ecaf0f661d3e5e97fd7895
Willmanu <pamanuwill@gmail.com>
2023-03-08 15:07:11 -0300

    bundle não funcionava no projeto novo

--- 3criandoumprojeto/criandoumprojeto.rb
+++ 3criandoumprojeto/criandoumprojeto.rb
@@ -8,11 +8,13 @@ gems installed rails
 Após esse comando, o Rails baixa todas as dependências, as instala e instala também a biblioteca
 Após essa instalação é possível criar um projeto
 Indo no terminal e escrevendo:
-rails new nome_projeto, dando enter nesta linha, o rails cria as estruturas de diretórios dessa
+rails new nome_projeto
+
+Dando enter nesta linha, o rails cria as estruturas de diretórios dessa
 aplicação
-Cria-se um diretório com varias pastas e em uma delas, a bin, entrando nela é possível
-inicializar o servidor com o comando:
-bin/rails server
+Como vimos cria-se um diretório com varias pastas.
+Entre essas pastas temos a bin e  entrando nela é possível inicializar o servidor com o comando:
+bin/rails server 
 
 bin é uns dos diretórios, como dito acima, que o rails criou
 rails é um dos scripts
