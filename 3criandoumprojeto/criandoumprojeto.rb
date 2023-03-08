31e8df836214e9ac67faae849a5565a716c403cc
Willmanu <pamanuwill@gmail.com>
2023-03-08 11:06:42 -0300

    Descrevendo o processo de criação de um novo projeto

--- 3criandoumprojeto/criandoumprojeto.rb
+++ 3criandoumprojeto/criandoumprojeto.rb
@@ -10,6 +10,15 @@ Após essa instalação é possível criar um projeto
 Indo no terminal e escrevendo:
 rails new nome_projeto, dando enter nesta linha, o rails cria as estruturas de diretórios dessa
 aplicação
-Cria-se um d
+Cria-se um diretório com varias pastas e em uma delas, a bin, entrando nela é possível
+inicializar o servidor com o comando:
+bin/rails server
+
+bin é uns dos diretórios, como dito acima, que o rails criou
+rails é um dos scripts
+ server é o comando para servir a aplicação
+ Após rodar esse comando o rails inicializa o servidor web e mostra a porta que esta escutando,
+ o caminho etc.
+ Com esse caminho escrito no browser, será possível ver essa nova aplicação
 
 =end
