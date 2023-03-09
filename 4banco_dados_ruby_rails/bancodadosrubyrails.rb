e0b0d6d81f799d74a8c395895a2459178fa4d71b
Willmanu <pamanuwill@gmail.com>
2023-03-09 12:43:38 -0300

    Descrevendo o processo do BD

---
+++ 4banco_dados_ruby_rails/bancodadosrubyrails.rb
@@ -0,0 +1,22 @@
+=begin
+
+                                       Banco de dados com Rails
+Anteriormente vimos com criar um projeto com rails bem simples e fácil
+Agora vamos criar tabelas e brincar com o terminal do rails
+
+Não especifiquei com qual BD iria trabalhar com o Rails como servidor. Isso fez que o
+Rails por padrão especificasse dentro de si o BD SQLite3
+
+A forma de indicar o BD que seria usado no Rails quando criar o projeto, como servidor é:
+rails new nome_projeto -d nome_BD(mysql, postgresql etc.)
+ ou seja, o script para criar o projeto mais a flag -d e o BD
+ Como não especifiquei o banco quando criei o projeto, ficou o padrão sqlite3
+
+para conferir o BD que esta no projeto use:
+cat config/database.yml
+
+cat é o comando para mostrar o conteúdo de um arquivo
+
+No casse o conteúdo que seja mostrado é do arquivo database.yml que esta na pasta config
+dentro do projeto. Neste arquivo esta especificado qual o BD que esta configurado.
+=end
