d11c45ed595a55d60bbfb1bed3361c3a9ec89d5c
Willmanu <pamanuwill@gmail.com>
2023-03-18 16:17:49 -0300

    Começando curso MVC

---
+++ 4MVC/mvc.rb
@@ -0,0 +1,15 @@
+=begin
+
+Model
+View
+Controller
+
+É um padrão de arquitetura de software que separa a representação da informação da interação do
+usuário
+
+Ela foi criada para resolver um problema
+
+
+
+
+=end
