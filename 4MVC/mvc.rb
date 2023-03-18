4f6299c67f68f264b6c77e00c6dd85afa98d9f4c
Willmanu <pamanuwill@gmail.com>
2023-03-18 16:45:03 -0300

    Começando curso MVC

--- 4MVC/mvc.rb
+++ 4MVC/mvc.rb
@@ -7,9 +7,22 @@ Controller
 É um padrão de arquitetura de software que separa a representação da informação da interação do
 usuário
 
+                                   Problema que o MVC resolve
+
 Ela foi criada para resolver um problema
+E qual seria?
 
+Quando se cria um aplicação web, esta é colocada em um servidor.
+Uma pessoa vai acessar o browser e vai acessar essa aplicação que esta em um servidor
+Essa aplicação pode ser feita em um único arquivo
+Porém quando se começa a criar novas funcionalidades para esse arquivo a tendencia é
+ele ir aumentando
+Vai inchando inchando até se tornar uma bola de neve
+Este fato quer dizer que se criar uma aplicação e ela for crescendo de forma desorganizada,
+e muito fácil ter um problema
+Principalmente em uma aplicação que atende muitas pessoas, será normal precisar criar novas
+funcionalidades e assim essa aplicação crescer.
+E com isso muita manutenção no futuro
 
-
-
+Ai que entra o MVC para resolver esse problema
 =end
