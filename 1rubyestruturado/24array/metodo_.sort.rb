c3cc9c6f4867e2790a2c654d960f92df9b4cc32d
Willmanu <pamanuwill@gmail.com>
2023-04-14 09:53:36 -0300

    Falando sobre o método .sort em array

---
+++ 1rubyestruturado/24array/metodo_.sort.rb
@@ -0,0 +1,13 @@
+=begin
+
+                                           Método .sort
+ É um metodo muito usado para ordenar elementos em um array ou Range, em ordem crescente ou
+ decrescente dependendo do argumento passado para ele
+ Ele retorna uma nova matriz que contém os elementos na ordem especificada
+ Exemplo:
+
+=end
+
+array = [5, 2, 8, 1, 3]
+number_sorted = array.sort
+puts number_sorted
